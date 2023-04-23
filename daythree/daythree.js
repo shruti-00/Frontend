eb6d6938478cd33be32838501d531c6e77c5e1be
shruti-00 <shrutisalve055@gmail.com>
2023-04-23 22:43:28 +0530

    Third Commit

---
+++ daythree/daythree.js
@@ -0,0 +1,46 @@
+alert("Congratulations")
+alert("Hello")
+alert(4+2)
+
+// var num=15;
+//     alert(num)
+// num=167;    
+//     alert(num)
+// var num=15;
+//     alert(num)
+// const a =123;
+//     alert(a);
+// a=122;
+//     alert(a);
+
+//assignment operator
+let  x=10; 
+    x*=20;
+    alert(x)
+
+var a=4;
+    b=4;
+    alert(a*=b)
+
+//relational operator
+// var res=12 ==0;
+//     alert(res)
+
+//logical operator
+var p=13;
+var q=12;
+alert(p==q && p>q)
+
+//if else
+
+if(p>q)
+    alert("Shruti")
+else
+    alert("Salve")
+
+    //typeof operator
+alert(typeof(true))
+
+let age =prompt("whats your age?",100)
+alert("your age is :" +age)
+
---
+++ daythree/intro.js
@@ -0,0 +1,6 @@
+function getLength()
+{
+    var element=document.getElementById('name');
+    var length=element.value.length;
+    alert(length);
+}
